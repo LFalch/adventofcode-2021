56799c4466d99de1f5956981e8fc4e14a8082a18
LFalch <lucas@wasd.dk>
2021-12-01 13:46:15 +0100

    Day 1 part 2

--- day1/main.c
+++ day1/main.c
@@ -10,5 +10,23 @@ int main() {
         if (next > last_num) increases++;
         last_num = next;
     }
-    printf("%u\n", increases);
+    printf("Indidviual increases: %u\n", increases);
+    fclose(f);
+
+    f = fopen("input.txt", "r");
+    increases = 0;
+    unsigned int last_last_last_num, last_last_num;
+    fscanf(f, "%u", &last_last_last_num);
+    fscanf(f, "%u", &last_last_num);
+    fscanf(f, "%u", &last_num);
+    while (fscanf(f, "%u", &next) == 1) {
+        unsigned last_sum = last_last_last_num + last_last_num + last_num;
+        unsigned next_sum = last_last_num + last_num + next;
+        if (next_sum > last_sum) increases++;
+        last_last_last_num = last_last_num;
+        last_last_num = last_num;
+        last_num = next;
+    }
+    printf("Window3 increases: %u\n", increases);
+    fclose(f);
 }
